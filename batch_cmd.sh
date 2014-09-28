883805c5fed3dc1708e0978f160bf0f0d67d608b
nuaazdh <luerfeng2012@gmail.com>
2014-09-28 17:08:18 +0800

    Update batch_cmd.sh
    small change: Line 16:  string -> variable

--- batch_cmd.sh
+++ batch_cmd.sh
@@ -4,19 +4,19 @@ if [ "$#" -ne 2 ] ; then
     echo "USAGE: $0 -f server_list_file cmd"         
     exit -1                                          
 fi                                                   
-                                                     
+
 file_name=$1                                         
 cmd_str=$2                                           
-                                                     
+ 
 cwd=$(pwd)                                           
 cd $cwd                                              
 serverlist_file="$cwd/$file_name"                    
-                                                     
+
 if [ ! -e $serverlist_file ] ; then                  
-    echo 'server.list not exist';                    
+    echo "$serverlist_file not exist";                    
     exit 0                                           
 fi                                                   
-                                                     
+
 while read line                                      
 do                                                   
     #echo $line                                      
